997792608d8d790de5d6b1d077209ec7db41a5a8
shizaoffical <110219012+shizaoffical@users.noreply.github.com>
2022-10-03 08:25:00 -0700

    1st lecture

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar'
+import Footer from './component/Footer';
+import Container from './component/Container';
+function App() {
+  return (
+    <>
+    <div>
+   
+    <Navbar/>
+    <Container/>
+    <Footer/>
+    <Footer/>
+
+    </div>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function  abc(){
+    return(
+        <>
+<nav>
+    <div className="logo">Logo Hear</div>
+    <div className="main-nav">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">services</a></li>
+            <li><a href="">contect</a></li>
+        </ul>
+    </div>
+    <div className="btn"><button>submit</button></div>
+</nav>
+</>
+    )
+}
+export default abc;
